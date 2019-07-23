19da6403429b2968904f1b6f30eba7932a5d986b
1173710106 <1171845717@qq.com>
2019-07-23 20:32:20 +0800

    refresh

---
+++ src/main/java/com/neusoft/root/MD5.java
@@ -0,0 +1,16 @@
+package com.neusoft.root;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+public class MD5 {
+	public static String toString(String string) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        // 确定计算方法
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        Base64.Encoder base64Encoder = Base64.getEncoder();
+        // 加密字符串
+        return base64Encoder.encodeToString(md5.digest(string.getBytes("utf-8")));
+    }
+}
--- src/main/java/com/neusoft/root/controller/LoginController.java
+++ src/main/java/com/neusoft/root/controller/LoginController.java
@@ -1,5 +1,8 @@
 ﻿package com.neusoft.root.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.neusoft.root.MD5;
+
 
 
 @Controller
@@ -15,9 +20,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 	@RequestMapping(value="checkAccount", method=RequestMethod.POST, produces="application/json;charset=UTF-8")
 	@ResponseBody
-	public String checkAccount(HttpServletRequest req, String username, String password) {
+	public String checkAccount(HttpServletRequest req, String username, String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		System.out.println("用户名：" + username + "  密    码：" + password);
-		if(username.equals("admin") && password.equals("123")){
+		String password_md5 = MD5.toString("123");
+		if(username.equals("admin") && password.equals(password_md5)){
 			//建立会话对象，存储登录状态
 			HttpSession session = req.getSession();
 			session.setAttribute("flag",username);
