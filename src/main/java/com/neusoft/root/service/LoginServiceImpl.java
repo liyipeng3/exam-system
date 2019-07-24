c42e8b82f069c9d4e05b7f03c50e2bb1184db03f
1173710107 <46263800+1173710107@users.noreply.github.com>
2019-07-24 10:06:36 +0800

    new2

--- src/main/java/com/neusoft/root/service/LoginServiceImpl.java
+++ src/main/java/com/neusoft/root/service/LoginServiceImpl.java
@@ -1,8 +1,36 @@
 package com.neusoft.root.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import com.neusoft.root.dao.Identification;
 
 @Service
 public class LoginServiceImpl implements LoginService{
+	@Autowired
+	private Identification identify;
+
+	@Override
+	public int querylogin(String id, String password) {
+		// TODO Auto-generated method stub
+		if(identify.queryadminIdentify(id, password))
+		{
+			return 1;
+		}
+		else if(identify.querystudentIdentify(id, password))
+		{
+			return 2;
+		}
+		else if(identify.queryteacherIdentify(id, password))
+		{
+			return 3;
+		}
+		else{
+			return 0;
+		}
+		
+	}
+
 	
 }
