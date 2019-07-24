698b3c3e54097f9757c2e665e6610d898635d150
1173710105 <1249614654@qq.com>
2019-07-24 15:48:01 +0800

    zyc-2019年7月24日15:47:11

--- src/main/java/com/neusoft/root/domain/Item.java
+++ src/main/java/com/neusoft/root/domain/Item.java
@@ -10,7 +10,7 @@ public class Item {
 	private String itemOption; //试题问题
 	private String itemAnswer; //试题答案
 	private String itemPicture; //试题路径
-	private Double itemScore; //提示分数
+	private Double itemScore; //提示分数 
 	
 	@Override
 	public int hashCode() {
