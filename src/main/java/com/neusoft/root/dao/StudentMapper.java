e1687c196b9cc587b2bb1be9152f32ac3822ad12
1173710107 <46263800+1173710107@users.noreply.github.com>
2019-07-24 19:21:39 +0800

    studentmapper

--- src/main/java/com/neusoft/root/dao/StudentMapper.java
+++ src/main/java/com/neusoft/root/dao/StudentMapper.java
@@ -3,11 +3,16 @@ package com.neusoft.root.dao;
 import java.util.List;
 import java.util.Map;
 
+import com.mysql.cj.protocol.Message;
 import com.neusoft.root.domain.Class1;
 import com.neusoft.root.domain.Course;
 import com.neusoft.root.domain.Courseteacher;
 import com.neusoft.root.domain.Forum;
 import com.neusoft.root.domain.Log;
+import com.neusoft.root.domain.Paper;
+import com.neusoft.root.domain.Result;
+import com.neusoft.root.domain.Student;
+import com.neusoft.root.domain.Teacher;
 
 public interface StudentMapper {
 	
@@ -28,4 +33,21 @@ public interface StudentMapper {
 	//log
 	public void  addLog(Log plog);
 	public List<Object> queryLog(Log plog);
+	
+	//对消息进行增删查操作
+	public void addMessage(Message message);
+	public void deleteMessage(Message message);
+	public List<Message> queryMessage(Message message);
+	
+	//对试卷查看
+	public List<Paper> queryPaper(Paper paper);
+	
+	//答题result
+	public void addResult(Result result);
+	public void updateResult(Result result);
+	
+	//学生
+	//改密码
+	public void updateStudent(String id,String password);
+	public List<Student> querystudent(Student student);
 }
