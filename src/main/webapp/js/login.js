658ca052c9791102549307d9bbe53e94d2e4d7c3
unknown <17895987@qq.com>
2019-07-24 10:28:01 +0800

    add teacher

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -84,74 +84,48 @@ function userLogin() {
         var dataForm, password;
         var nextUrl = getQueryString("nextUrl");
 
-        if (domain != "") {
-            //独立入口
-            if (filter1.test(username) || filter2.test(username)) {
-                $("#username_tran").val(username);
-            } else {
-                $("#username_tran").val(username + '@' + domain);
-            }
 
-            dataForm = $("#loginForm [type!=password]").serialize();
-            password = $("#loginForm [name=password]").val();
-
-            dataForm += "&password=" + md5(password) + "&nextUrl=" + nextUrl;
-            $("#loginBtn").addClass("disabled");
-            $.ajax({
-                type: "POST",
-                cache: "false",
-                headers: {"cache-control": "no-cache"},
-                dataType: "text",
-                url: "/sm/login/checkAccount",
-                data: dataForm,
-                success: function (msg) {
-                    if (msg==="ok") {
-                        {
-                            window.location.href = "/sm/index";
-                        }
-                    } else {
-                        $("#loginBtn").removeClass("disabled");
-                        $("#errormsg").text(msg);
-                    }
-                },
-                error:function(msg){
-                    console.log(msg)
-                }
-            })
-        } else {
-            //公共入口
+        if (filter1.test(username) || filter2.test(username)) {
             $("#username_tran").val(username);
-            dataForm = $("#loginForm [type!=password]").serialize();
-            password = $("#loginForm [name=password]").val();
-
-            dataForm += "&password=" + md5(password);
-
-            if (filter2.test(username) || filter3.test(username) || (/^\d{11}$/.test(username))) {
-                $("#loginBtn").addClass("disabled");
-                $.ajax({
-                    type: "POST",
-                    cache: "false",
-                    headers: {"cache-control": "no-cache"},
-                    dataType: "text",
-                    url: "/sm/login/checkAccount",
-                    data: dataForm,
-                    success: function (msg) {
-                        if (msg==="ok") {
-                            window.location.href = "/sm/index";
-                        } else {
-                            $("#loginBtn").removeClass("disabled");
-                            $("#errormsg").text(msg);
-                        }
-                    },
-                })
-            } else {
-                if (username == 'admin') {
-                    $("#errormsg").text('请补全账号后缀');
-                } else {
-                    $("#errormsg").text('非管理员请从机构独立入口登录，独立入口请联系组织者获取');
-                }
-            }
+        } else {
+            $("#username_tran").val(username + '@' + domain);
         }
+
+        dataForm = $("#loginForm [type!=password]").serialize();
+        password = $("#loginForm [name=password]").val();
+
+        dataForm += "&password=" + md5(password) + "&nextUrl=" + nextUrl;
+        $("#loginBtn").addClass("disabled");
+        $.ajax({
+            type: "POST",
+            cache: "false",
+            headers: {"cache-control": "no-cache"},
+            dataType: "text",
+            url: "/sm/login/checkAccount",
+            data: dataForm,
+            success: function (msg) {
+                if (msg === "admin") {
+
+                    window.location.href = "/sm/index";
+
+                } else if (msg === "student") {
+
+                    window.location.href = "/sm/student"
+
+                } else if (msg === "teacher") {
+
+                    window.location.href = "/sm/teacher"
+                    
+                } else {
+                    $("#loginBtn").removeClass("disabled");
+                    $("#errormsg").text(msg);
+                }
+            },
+            error: function (msg) {
+                console.log(msg)
+            }
+        })
+
     }
 }
 
