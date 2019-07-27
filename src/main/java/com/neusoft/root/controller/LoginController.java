0c61491c6696db4fd602656543bc96c5e5a2d5a0
1173710107 <46263800+1173710107@users.noreply.github.com>
2019-07-27 15:17:44 +0800

    999

--- src/main/java/com/neusoft/root/controller/LoginController.java
+++ src/main/java/com/neusoft/root/controller/LoginController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.neusoft.root.MD5;
+import com.neusoft.root.service.LoginServiceImpl;
 
 @Controller
 @RequestMapping("/login")
