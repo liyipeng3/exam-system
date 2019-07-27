19b3e4c5a117abe416606fe9b4a2d64a0bb7c23a
unknown <17895987@qq.com>
2019-07-27 10:59:55 +0800

    Update PaperGeneratorController.java

--- src/main/java/com/neusoft/root/controller/PaperGeneratorController.java
+++ src/main/java/com/neusoft/root/controller/PaperGeneratorController.java
@@ -40,9 +40,9 @@ public class PaperGeneratorController {
 		long time = System.currentTimeMillis();
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		String datestring = df.format(time);
-		RawPaper paper1 = new RawPaper("1","test1", "1",datestring, "语文", 0.1,"choice", "fill", "subjective", "hhh", 100.0, "sss", "sss");
-		RawPaper paper2 = new RawPaper("2","test2","2", datestring, "语文", 0.2, "choice", "fill", "subjective", "hhhh", 100.0, "sss", "sss");
-		RawPaper paper3 = new RawPaper("3", "test3","3", datestring, "语文", 0.3, "choice", "fill", "subjective", "hhhhh", 100.0, "sss", "sss");
+		RawPaper paper1 = new RawPaper(1,"test1", "1",datestring, "语文", 0.1,"choice", "fill", "subjective", "hhh", 100.0, "sss", "sss");
+		RawPaper paper2 = new RawPaper(2,"test2","2", datestring, "语文", 0.2, "choice", "fill", "subjective", "hhhh", 100.0, "sss", "sss");
+		RawPaper paper3 = new RawPaper(3, "test3","3", datestring, "语文", 0.3, "choice", "fill", "subjective", "hhhhh", 100.0, "sss", "sss");
 		papers.add(paper1);
 		papers.add(paper2);
 		papers.add(paper3);
@@ -69,9 +69,10 @@ public class PaperGeneratorController {
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		String datestring = df.format(time);
 		List<RawItem> items = new ArrayList<>();
-		RawItem item1 = new RawItem("itemId", "itemCourseType", "itemType", 0.1, "itemQuestion", "itemOption", "itemAnswer", "itemPicture", 0.1);
-		RawItem item2 = new RawItem("题目Id", "科目", "题型", 0.2, "题干", "选项", "答案", "配图路径", 0.2);
-		RawItem item3 = new RawItem("1", "马克思主义原理", "送分题", 99.9, "老大帅不帅", "是/是", "是", "> A <", 99.9);
+		RawItem item1 = new RawItem(1, "10011",datestring,"itemCourseType", "itemType", 0.1, "itemQuestion", "itemOption", "itemAnswer", "itemPicture", 0.1);
+		RawItem item2 = new RawItem(2, "1008",datestring,"科目", "题型", 0.2, "题干", "选项", "答案", "配图路径", 0.2);
+		RawItem item3 = new RawItem(3, "1008611",datestring, "马克思主义原理", "送分题", 99.9, "老大帅不帅", "是/是", "是", "> A <", 99.9);
+		RawItem item4 = new RawItem(4, "10086", datestring, "语文", "单选题", 0.6,"老大帅不帅" , "是/是", "是",  "> A <", 99.9);
 		items.add(item1);
 		items.add(item2);
 		items.add(item3);
