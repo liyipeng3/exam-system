72ae37f396d1a32177cbf5a0f4430614b213b966
1173710106 <1171845717@qq.com>
2019-07-29 14:20:26 +0800

    h

--- src/main/java/com/neusoft/root/config/WebMvcConfig.java
+++ src/main/java/com/neusoft/root/config/WebMvcConfig.java
@@ -17,12 +17,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/css/**", "/js/**",
 				"/fonts/**", "/img/**", "/login*", "/login/**", "/register*", "/register/**","/exam*","/exam/**");
-		registry.addInterceptor(new AdminInterceptor()).addPathPatterns("/**").excludePathPatterns("/css/**", "/js/**",
+		/*registry.addInterceptor(new AdminInterceptor()).addPathPatterns("/**").excludePathPatterns("/css/**", "/js/**",
 				"/fonts/**", "/img/**", "/login*", "/login/**", "/register*", "/register/**", "/teacher*", "/teacher/**", "/student*", "/student/**","/exam*","/exam/**");
 		registry.addInterceptor(new TeacherInterceptor()).addPathPatterns("/**").excludePathPatterns("/css/**", "/js/**",
 				"/fonts/**", "/img/**", "/login*", "/login/**", "/register*", "/register/**", "/index*", "/index/**", "/student*", "/student/**","/exam*","/exam/**");
 		registry.addInterceptor(new StudentInterceptor()).addPathPatterns("/**").excludePathPatterns("/css/**", "/js/**",
-				"/fonts/**", "/img/**", "/login*", "/login/**", "/register*", "/register/**", "/index*", "/index/**", "/teacher*", "/teacher/**","/exam*","/exam/**");
+				"/fonts/**", "/img/**", "/login*", "/login/**", "/register*", "/register/**", "/index*", "/index/**", "/teacher*", "/teacher/**","/exam*","/exam/**");*/
 	}
     @Override
     public void addCorsMappings(CorsRegistry registry) {
