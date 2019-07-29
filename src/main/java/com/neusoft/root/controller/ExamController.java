db11fee2b24e6e25ec11cc1dceefe9291d71fd90
1173710106 <1171845717@qq.com>
2019-07-29 14:07:36 +0800

    h

--- src/main/java/com/neusoft/root/controller/ExamController.java
+++ src/main/java/com/neusoft/root/controller/ExamController.java
@@ -80,7 +80,7 @@ public class ExamController {
 	@ResponseBody
 	public String getItems(HttpServletRequest request){
 		HttpSession session = request.getSession();
-		session.getAttribute("username");
+		String username = session.getAttribute("username").toString();
 		long time = System.currentTimeMillis();
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 		String datestring = df.format(time);
@@ -88,7 +88,7 @@ public class ExamController {
 		RawItem item1 = new RawItem(1, "10011",datestring,"itemCourseType", "itemType", 0.1, "itemQuestion", "itemOption", "itemAnswer", "itemPicture", 0.1,"");
 		RawItem item2 = new RawItem(2, "1008",datestring,"科目", "题型", 0.2, "题干", "选项", "答案", "配图路径", 0.2,"");
 		RawItem item3 = new RawItem(3, "1008611",datestring, "马克思主义原理", "送分题", 99.9, "老大帅不帅", "是/是", "是", "> A <", 99.9,"");
-		RawItem item4 = new RawItem(4, "10086", datestring, "语文", "单选题", 0.6,"老大帅不帅" , "是/是", "是",  "> A <", 99.9,"");
+		RawItem item4 = new RawItem(4, username, datestring, "语文", "单选题", 0.6,"老大帅不帅" , "是/是", "是",  "> A <", 99.9,"");
 		items.add(item1);
 		items.add(item2);
 		items.add(item3);
@@ -98,8 +98,14 @@ public class ExamController {
 	}
 	@RequestMapping(value="/add_item",method=RequestMethod.POST)
 	@ResponseBody
-	public String addItem(@RequestBody JSONObject jsonParam){
-		System.out.println("6666666");
+	public String addItem(@RequestBody JSONObject jsonParam, HttpServletRequest request){
+		HttpSession session = request.getSession();
+		String username = session.getAttribute("username").toString();
+		long time = System.currentTimeMillis();
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		String date = df.format(time);
+		jsonParam.put("createrId", username);
+		jsonParam.put("itemDate", date);
 		System.out.println(jsonParam.toString());
 		return "ok";
 	}
