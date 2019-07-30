5109ba931515f559d7550a15f4bfc3ca1a59d6fe
1173710107 <46263800+1173710107@users.noreply.github.com>
2019-07-30 21:21:48 +0800

    new

--- src/main/java/com/neusoft/root/service/ResultServiceImpl.java
+++ src/main/java/com/neusoft/root/service/ResultServiceImpl.java
@@ -19,15 +19,51 @@ public class ResultServiceImpl implements ResultService{
 	@Override
 	public void addResult(JSONObject json) {
 		// TODO Auto-generated method stub
-		//Result result = new Result(json.getString("studentId"), json.getInteger("paperId"), json.getString("singlechoiceResult"), json.getString("multichoiceResult"), json.getString("fillResult"), json.getString("subjectiveResult"), json.getString("submitDate"));
-		//mapper.addResult(result);
+		int i=0;
+		String singlechoiceResult ="";
+		while(json.getString("singlechoiceResult"+i)!=null)
+		{
+			singlechoiceResult = singlechoiceResult+json.getString("singlechoiceResult"+i)+","+json.getDouble("score"+i)+"###";
+			i++;
+		}
+		if(singlechoiceResult!="")
+		singlechoiceResult = singlechoiceResult.substring(0, singlechoiceResult.length()-3);
+		i=0;
+		String multichoiceResult ="";
+		while(json.getString("multichoiceResult"+i)!=null)
+		{
+			multichoiceResult = multichoiceResult+json.getString("multichoiceResult"+i)+","+json.getDouble("score"+i)+"###";
+			i++;
+		}
+		if(multichoiceResult!="")
+			multichoiceResult = multichoiceResult.substring(0,multichoiceResult.length()-3);
+		i=0;
+		String fillResult ="";
+		while(json.getString("fillResult"+i)!=null)
+		{
+			fillResult = fillResult+json.getString("fillResult"+i)+","+json.getDouble("score"+i)+"###";
+			i++;
+		}
+		if(fillResult!="")
+			fillResult = fillResult.substring(0,fillResult.length()-3);
+		i=0;
+		String subjectiveResult ="";
+		while(json.getString("subjectiveResult"+i)!=null)
+		{
+			subjectiveResult = subjectiveResult+json.getString("subjectiveResult"+i)+","+json.getDouble("score"+i)+"###";
+			i++;
+		}
+		if(subjectiveResult!="")
+			subjectiveResult = subjectiveResult.substring(0,subjectiveResult.length()-3);
+		RawResult result = new RawResult(json.getString("studentId"), json.getInteger("paperId"), singlechoiceResult, multichoiceResult, fillResult, subjectiveResult, json.getString("submitDate"),json.getString("checked"));
+		mapper.addResult(result);
 	}
 
 
 	@Override
 	public List<RawResult> queryRawResult(JSONObject json) {
 		// TODO Auto-generated method stub
-		//Result result = new Result(json.getString("studentId"), json.getInteger("paperId"), json.getString("singlechoiceResult"), json.getString("multichoiceResult"), json.getString("fillResult"), json.getString("subjectiveResult"), json.getString("submitDate"));
+	//	Result result = new Result(json.getString("studentId"), json.getInteger("paperId"), json.getString("singlechoiceResult"), json.getString("multichoiceResult"), json.getString("fillResult"), json.getString("subjectiveResult"), json.getString("submitDate"));
 		//List<Result> list = mapper.queryResult(result);
 		return null;
 	}
