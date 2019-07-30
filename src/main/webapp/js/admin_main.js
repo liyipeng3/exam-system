685ba6889c4af3a8295567e12fcc92041c29f67c
unknown <17895987@qq.com>
2019-07-30 13:59:42 +0800

    修改网页

--- src/main/webapp/js/admin_main.js
+++ src/main/webapp/js/admin_main.js
@@ -1,24 +1,4 @@
 $(document).ready(function () {
-    //读取用户自定义LOGO
-    if ($("#logo").length > 0) {
-        $.ajax({
-            type: "GET",
-            cache: false,
-            //headers: { "cache-control": "no-cache" },
-            dataType: "json",
-            url: "/admin/modify_get_logo",
-            success: function (msg) {
-                if (msg.success == true) {
-                    var logoUrl = msg.bizContent.logoUrl;
-                    var img = '<img src="' + logoUrl + '" style="height:47px;" />';
-                    $("#logo a").css("background-image", "none").html(img);
-                    $("#logo").show();
-                } else {
-                    //alert("操作失败，请稍候再试！");
-                }
-            }
-        });
-    }
 
     // 退出登录(清空cookie,session&&sessionId)
     $("#logoutBtn").click(function (e) {
